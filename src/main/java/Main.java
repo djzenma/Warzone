37ee72a6a97876aa950c199acb5a25e7bd7841fd
djzenma <eidma@aucegypt.edu>
2021-02-10 16:23:08 -0500

    Fixed Bug in Reinforcement Phase
    Signed-off-by: djzenma <eidma@aucegypt.edu>

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,5 @@
-import Controller.GameEngineController;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        GameEngineController gameEngine = new GameEngineController();
-
-        gameEngine.addPlayer("Mazen");
-        gameEngine.addPlayer("Aman");
-        gameEngine.addPlayer("Akshat");
-
-        gameEngine.assignCountries();
-        gameEngine.assignReinforcements();
-
-        try {
-            gameEngine.removePlayer("Mazen");
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
     }
 }
