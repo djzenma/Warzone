cde0a52ffe1cc345a53fae6bebb15871a16d326b
Amanjotkaur98 <amanjot952@gmail.com>
2021-02-22 19:44:07 +0530

    Added the JUnit Test Suite.
    Signed-off-by: Amanjotkaur98 <amanjot952@gmail.com>

---
+++ src/test/java/JUnitTestSuite.java
@@ -0,0 +1,16 @@
+import Controller.PlayerControllerTest;
+import Model.GameEngineModelTest;
+import Model.MapModelTest;
+import Utils.CommandsParserTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({GameEngineModelTest.class,
+        PlayerControllerTest.class,
+        GameEngineModelTest.class,
+        MapModelTest.class,
+        CommandsParserTest.class})
+
+public class JUnitTestSuite {
+}
